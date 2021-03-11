fb5f701bca644d03ec7ec4ebcf28695cd2d5baa5
Hogar Maldonado <Hogar Maldonado@DESKTOP-J643LCQ>
2021-03-11 19:16:15 +0100

    Pushing JUnit Tests

--- src/test/java/com/microservices/namingserver/NamingServerApplicationTests.java
+++ src/test/java/com/microservices/namingserver/NamingServerApplicationTests.java
@@ -2,6 +2,11 @@ package com.microservices.namingserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.net.InetAddress;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class NamingServerApplicationTests {
@@ -10,4 +15,17 @@ class NamingServerApplicationTests {
 	void contextLoads() {
 	}
 
+	// Service must be running in background, otherwise unit test will fail
+	@Test
+	public void testEurekaNamingServer() throws Exception {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		String ipLocalHost = InetAddress.getLocalHost().getHostAddress();
+		// Capture GET response
+		ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:8761", String.class);
+
+		// Confirm Successful Status Code in response
+		assertTrue(response.getStatusCode().is2xxSuccessful());
+		// Confirm Response contains IP of Local Host
+		assertTrue(response.getBody().contains(ipLocalHost));
+	}
 }
